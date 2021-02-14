9de214ea67948b7a7cf0dd6482704d4d99e83e56
Thammasok <jaranchai.nt@hotmail.com>
2021-02-14 22:28:50 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,4 +3,5 @@ const Blockchain = require('./blockchain')
 blockchain = new Blockchain()
 blockchain.addBlock({ data: 'foo' })
 blockchain.addBlock({ data: 'bar' })
+
 console.log('blockchain', blockchain)
