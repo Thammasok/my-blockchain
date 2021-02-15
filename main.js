7fa09cae60db809d84ca48ca37a2797ca360a46e
Thammasok <jaranchai.nt@hotmail.com>
2021-02-15 07:32:56 +0700

    express api

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
 const Blockchain = require('./blockchain')
 
+const app = express()
 blockchain = new Blockchain()
-blockchain.addBlock({ data: 'foo' })
-blockchain.addBlock({ data: 'bar' })
 
-console.log('blockchain', blockchain)
+app.use(bodyParser.json())
+
+app.get('/api/blocks', (req, res) => {
+  res.json(blockchain.chain)
+})
+
+app.post('/api/mine', (req, res) => {
+  const { data } = req.body
+  blockchain.addBlock({ data })
+
+  res.redirect('/api/blocks')
+})
+
+app.listen(3000, () => console.log(`listening at localhost:3000`))
